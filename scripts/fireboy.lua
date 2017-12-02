5a60ceb43a718499661a2504ebf9d906ee5b1395
Gabriel Naves da Silva <gabri.navess@gmail.com>
2017-12-02 17:36:07 -0200

    Updating fireboy launch

--- scripts/fireboy.lua
+++ scripts/fireboy.lua
@@ -6,6 +6,8 @@ fireboy.y = 608
 fireboy.movSpeed = 10
 fireboy.jumpSpeed = 25
 fireboy.launchSpeed = 500
+fireboy.velX, fireboy.velY = 0, 0
+fireboy.accX, fireboy.accY = 0, 0
 
 -- Image data
 fireboy.img = love.graphics.newImage('assets/sprites/fireboy.png')
@@ -21,23 +23,34 @@ fireboy.frames = {
     love.graphics.newQuad(fireboy.frame_width * 4, 0, fireboy.frame_width, fireboy.frame_height, fireboy.img:getWidth(), fireboy.img:getHeight())
 }
 
--- Animation timers
-fireboy.animationTimer = 0
+-- Timers
+fireboy.elapsedTime = 0
 fireboy.frameTime = 0.3
+fireboy.launchTime = 3
 
 function fireboy.draw(dt)
     love.graphics.draw(fireboy.img, fireboy.frames[fireboy.pos_frame],
                        fireboy.x - fireboy.frame_width / 2, fireboy.y - fireboy.frame_height)
 end
 
+function fireboy.updatePosition(dt)
+    fireboy.x = fireboy.x + fireboy.velX * dt
+    fireboy.y = fireboy.y - fireboy.velY * dt
+end
+
+function fireboy.updateVelocity(dt)
+    fireboy.velX = fireboy.velX + fireboy.accX * dt
+    fireboy.velY = fireboy.velY + fireboy.accY * dt
+end
+
 function fireboy.updateInitialState(dt, gamestate)
     -- Animation update
-    fireboy.animationTimer = fireboy.animationTimer + dt
-    if fireboy.animationTimer > fireboy.frameTime then
+    fireboy.elapsedTime = fireboy.elapsedTime + dt
+    if fireboy.elapsedTime > fireboy.frameTime then
         if fireboy.pos_frame == 1 then fireboy.pos_frame = 2
         elseif fireboy.pos_frame == 2 then fireboy.pos_frame = 1
         end
-        fireboy.animationTimer = 0
+        fireboy.elapsedTime = 0
     end
     -- State update
     if love.keyboard.isDown('space') then
@@ -49,7 +62,16 @@ end
 
 function fireboy.updateLaunch(dt, gamestate)
     fireboy.pos_frame = 3
-    fireboy.y = fireboy.y - fireboy.launchSpeed * dt
+    -- Position update
+    fireboy.velY = fireboy.launchSpeed
+    fireboy.updatePosition(dt)
+    -- State update
+    fireboy.elapsedTime = fireboy.elapsedTime + dt
+    if fireboy.elapsedTime > fireboy.launchTime then
+        fireboy.state = fireboy.states.float
+        fireboy.updateFunction = fireboy.updateFloat
+        fireboy.elapsedTime = 0
+    end
 end
 
 function fireboy.updateFloat(dt, gamestate)
